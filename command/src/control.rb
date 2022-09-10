ffeb6aeb9b75ac80ac4e9d5ddf8fc7589d654dc6
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-09-10 18:36:58 -0500

    Update control.rb

--- command/src/control.rb
+++ command/src/control.rb
@@ -16,26 +16,26 @@ class RemoteControlWithUndo
       end
       @undo_command = no_command
     end
-  
+
     def set_command(slot, on_command, off_command)
       @on_commands[slot] = on_command
       @off_commands[slot] = off_command
     end
-  
+
     def on_button_was_pushed(slot)
       @on_commands[slot].execute
       @undo_command = @on_commands[slot]
     end
-  
+
     def off_button_was_pushed(slot)
       @off_commands[slot].execute
       @undo_command = @off_commands[slot]
     end
-  
+
     def undo_button_was_pushed()
       @undo_command.undo
     end
-  
+
     def inspect
       string_buff = ["\n------ Remote Control -------\n"]
       @on_commands.zip(@off_commands) \
@@ -48,38 +48,58 @@ class RemoteControlWithUndo
       string_buff << "[undo] #{@undo_command.class}\n"
       string_buff.join
     end
-  
-  end
-  
+
+end
+
   class NoCommand
-  
-    def execute
-    end
-  
-    def undo
-    end
-  
+
+  def execute
   end
-  
+  def undo
+  end
+
+  end
+class LightOnCommand
+    def initialize(light_on)
+      @light = light_on
+    end
+    def execute
+      @light.on
+    end
+    def undo
+      @light.off
+    end
+end
+class LightOffCommand
+  def initialize(light_off)
+    @light_off = light_off
+  end
+  def execute
+    @light_off.off
+  end
+  def undo
+    @light_off.on
+  end
+end
   class Light
-  
+
     attr_reader :level
-  
+
     def initialize(location)
       @location = location
       @level = 0
     end
-  
+
     def on
       @level = 100
       puts "Light is on"
     end
-  
+
     def off
       @level = 0
       puts "Light is off"
     end
-  
+
     def dim(level)
       @level = level
       if level == 0
@@ -88,43 +108,57 @@ class RemoteControlWithUndo
         puts "Light is dimmed to #{@level}%"
       end
     end
-  
+
   end
-  
+class CeilingFanHighCommand
+  def initialize(fan)
+    @fan = fan
+  end
+end
+class CeilingFanMediumCommand
+  def initialize(fan)
+    @fan = fan
+  end
+end
+class CeilingFanOffCommand
+  def initialize(fan)
+    @fan = fan
+  end
+end
   class CeilingFan
-  
+
     # Access these constants from outside this class as
     # CeilingFan::HIGH, CeilingFan::MEDIUM, and so on.
     HIGH   = 3
     MEDIUM = 2
     LOW    = 1
     OFF    = 0
-  
+
     attr_reader :speed
-  
+
     def initialize(location)
       @location = location
       @speed = OFF
     end
-  
+
     def high
       @speed = HIGH
       puts "#{@location} ceiling fan is on high"
     end
-  
+
     def medium
       @speed = MEDIUM
       puts "#{@location} ceiling fan is on medium"
     end
-  
+
     def low
       @speed = LOW
       puts "#{@location} ceiling fan is on low"
     end
-  
+
     def off
       @speed = OFF
       puts "#{@location} ceiling fan is off"
     end
-  
+
   end
