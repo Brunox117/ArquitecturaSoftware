729c580aba7f6865455edfa5223227d239f649b4
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-10-07 21:46:40 -0500

    Update jankenpon_test.rb

--- dsl/src/jankenpon_test.rb
+++ dsl/src/jankenpon_test.rb
@@ -1,3 +1,8 @@
+# Domain-Specific Language Pattern
+# Date: 22-Aug-2022
+# Authors:
+#          A01748931 Bruno Omar Jimenez Mancilla 
+#          A01748559 Roberto Castro Barrios 
 # File: jankenpon_test.rb
 
 require 'minitest/autorun'
