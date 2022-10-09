eb8b63f25c7dcc8f18310c449bfe5401e6efbec2
Brunox117 <46934797+Brunox117@users.noreply.github.com>
2022-10-09 13:56:29 -0500

    Update jankenpon.rb

--- dsl/src/jankenpon.rb
+++ dsl/src/jankenpon.rb
@@ -4,46 +4,47 @@
 #          A01748931 Bruno Omar Jimenez Mancilla 
 #          A01748559 Roberto Castro Barrios 
 # File: jankenpon.rb
-# Abstract class of jankenpon to provide a common interface.
+#The Jankenpon file shows an implementation of the dsl pattern
+# Abstract class of jankenpon.
 class Jankenpon
-    # Abstract, overridable method
-    def + next_play
+    # Abstract method
+    def + rpsls
     end
-    # Abstract, overridable method
-    def - next_play
+    # Abstract method
+    def - rpsls
     end
 end
-# Specific implementation of Rock in the DSL.
+#The class rock shows the winning and losing cases of itself in the game, overrides Jankenpon.
 class Rock < Jankenpon
-    # Returns the winning play
-    def + next_play
-        if(next_play == Scissors)
+    #AQUI ME QUEDE
+    def + rpsls
+        if(rpsls == Scissors)
             puts "Rock crushes Scissors (winner Rock)"
             return self
-        elsif (next_play == Lizard)
+        elsif (rpsls == Lizard)
             puts "Rock crushes Lizard (winner Rock)"
             return self
-        elsif (next_play == Rock)
+        elsif (rpsls == Rock)
             puts "Rock tie (winner Rock)"
             return self
         else
-            return next_play + self
+            return rpsls + self
         end
     end
 
     # Return the losing play
-    def - next_play
-        if(next_play == Spock)
+    def - rpsls
+        if(rpsls == Spock)
             puts "Spock vaporizes Rock (loser Rock)"
             return self
-        elsif (next_play == Paper)
+        elsif (rpsls == Paper)
             puts "Paper covers Rock (loser Rock)"
             return self
-        elsif (next_play == Rock)
+        elsif (rpsls == Rock)
             puts "Rock tie (loser Rock)"
-            return next_play
+            return rpsls
         else
-            return next_play - self
+            return rpsls - self
         end
     end
     # Return the result.
@@ -56,34 +57,34 @@ Rock = Rock.new
 # Specific implementation of Paper in the DSL.
 class Paper < Jankenpon
     # Returns the winning play
-    def + next_play
-        if(next_play == Spock)
+    def + rpsls
+        if(rpsls == Spock)
             puts "Paper disproves Spock (winner Paper)"
             return self
-        elsif (next_play == Rock)
+        elsif (rpsls == Rock)
             puts "Paper covers Rock (winner Paper)"
             return self
-        elsif (next_play == Paper)
+        elsif (rpsls == Paper)
             puts "Paper tie (winner Paper)"
-            return next_play
+            return rpsls
         else
-            return next_play + self
+            return rpsls + self
         end
     end
 
     # Return the losing play
-    def - next_play
-        if(next_play == Lizard)
+    def - rpsls
+        if(rpsls == Lizard)
             puts "Lizard eats Paper (loser Paper)"
             return self
-        elsif (next_play == Scissors)
+        elsif (rpsls == Scissors)
             puts "Scissors cut Paper (loser Paper)"
             return self
-        elsif (next_play == Paper)
+        elsif (rpsls == Paper)
             puts "Paper tie (loser Paper)"
             return self
         else
-            return next_play - self
+            return rpsls - self
         end
     end
     # Return the result.
@@ -98,34 +99,34 @@ Paper = Paper.new
 # Specific implementation of Scissors in the DSL.
 class Scissors < Jankenpon
     # Returns the winning play
-    def + next_play
-        if(next_play == Paper)
+    def + rpsls
+        if(rpsls == Paper)
             puts "Scissors cut Paper (winner Scissors)"
             return self
-        elsif (next_play == Lizard)
+        elsif (rpsls == Lizard)
             puts "Scissors decapitate Lizard (winner Scissors)"
             return self
-        elsif (next_play == Scissors)
+        elsif (rpsls == Scissors)
             puts "Scissors tie (winner Scissors)"
-            return next_play
+            return rpsls
         else
-            return next_play + self
+            return rpsls + self
         end
     end
 
     # Return the losing play
-    def - next_play
-        if(next_play == Rock)
+    def - rpsls
+        if(rpsls == Rock)
             puts "Rock crushes Scissors (loser Scissors)"
             return self
-        elsif (next_play == Spock)
+        elsif (rpsls == Spock)
             puts "Spock smashes Scissors (loser Scissors)"
             return self
-        elsif (next_play == Scissors)
+        elsif (rpsls == Scissors)
             puts "Scissors tie (loser Scissors)"
-            return next_play
+            return rpsls
         else
-            return next_play - self
+            return rpsls - self
         end
     end
     # Return the result.
@@ -139,33 +140,33 @@ Scissors = Scissors.new
 # Specific implementation of Lizard in the DSL.
 class Lizard < Jankenpon
     # Returns the winning play
-    def + next_play
-        if(next_play == Spock)
+    def + rpsls
+        if(rpsls == Spock)
             puts "Lizard poisons Spock (winner Lizard)"
             return self
-        elsif (next_play == Paper)
+        elsif (rpsls == Paper)
             puts "Lizard eats Paper (winner Lizard)"
             return self
-        elsif (next_play == Lizard)
+        elsif (rpsls == Lizard)
             puts "Lizard tie (winner Lizard)"
             return self
         else
-            return next_play + self
+            return rpsls + self
         end
     end
     # Return the losing play
-    def - next_play
-        if(next_play == Rock)
+    def - rpsls
+        if(rpsls == Rock)
             puts "Rock crushes Lizard (loser Lizard)"
             return self
-        elsif (next_play == Scissors)
+        elsif (rpsls == Scissors)
             puts "Scissors decapitate Lizard (loser Lizard)"
             return self
-        elsif (next_play == Lizard)
+        elsif (rpsls == Lizard)
             puts "Lizard tie (loser Lizard)"
             return self
         else
-            return next_play - self
+            return rpsls - self
         end
     end
     # Return the result.
@@ -178,33 +179,33 @@ Lizard = Lizard.new
 # Specific implementation of Spock in the DSL.
 class Spock < Jankenpon
     # Returns the winning play
-    def + next_play
-        if(next_play == Scissors)
+    def + rpsls
+        if(rpsls == Scissors)
             puts "Spock smashes Scissors (winner Spock)"
             return self
-        elsif (next_play == Rock)
+        elsif (rpsls == Rock)
             puts "Spock vaporizes Rock (winner Spock)"
             return self
-        elsif (next_play == Spock)
+        elsif (rpsls == Spock)
             puts "Spock tie (winner Spock)"
             return self
         else
-            return next_play + self
+            return rpsls + self
         end
     end
     # Return the losing play
-    def - next_play
-        if(next_play == Paper)
+    def - rpsls
+        if(rpsls == Paper)
             puts "Paper disproves Spock (loser Spock)"
             return self
-        elsif (next_play == Lizard)
+        elsif (rpsls == Lizard)
             puts "Lizard poisons Spock (loser Spock)"
             return self
-        elsif (next_play == Spock)
+        elsif (rpsls == Spock)
             puts "Spock tie (loser Spock)"
             return self
         else
-            return next_play - self
+            return rpsls - self
         end
     end
 
